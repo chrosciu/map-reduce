787e80aaa9426232e9db8b786679e5053da68569
Marcin Chrost <mchrost82@gmail.com>
2023-05-11 10:08:50 +0200

    replaced pattern with string tokenizer

--- src/main/java/eu/chrost/mapreduce/wordcount/LineTokenizer.java
+++ src/main/java/eu/chrost/mapreduce/wordcount/LineTokenizer.java
@@ -1,14 +1,14 @@
 package eu.chrost.mapreduce.wordcount;
 
+import java.util.StringTokenizer;
 import java.util.function.Consumer;
-import java.util.regex.Pattern;
 
 public final class LineTokenizer {
-    private static final Pattern PATTERN = Pattern.compile("\\s|\\p{Punct}");
 
     public static void tokenizeLine(String line, Consumer<String> tokenConsumer) {
-        for (String token : PATTERN.split(line)) {
-            tokenConsumer.accept(token);
+        StringTokenizer tokenizer = new StringTokenizer(line, " \t\n\r\f!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+        while (tokenizer.hasMoreTokens()) {
+            tokenConsumer.accept(tokenizer.nextToken());
         }
     }
 }
